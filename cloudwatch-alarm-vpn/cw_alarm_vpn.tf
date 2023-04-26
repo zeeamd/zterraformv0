4005a36675a02f1c40b6f500a480545645f178ab
zee <zeeamd@dev.com>
2023-04-26 13:01:18 +0000

    add vpn tunnel cloudwatch alarm

---
+++ cloudwatch-alarm-vpn/cw_alarm_vpn.tf
@@ -0,0 +1,15 @@
+#vpn alarm
+resource "aws_cloudwatch_metric_alarm" "s2s_tunnel_status" {
+  alarm_name          = "vpn-tunnel-state-alarm"
+  comparison_operator = "LessThanOrEqualToThreshold"
+  metric_name         = "TunnelState"
+  namespace           = "AWS/VPN"
+  statistic           = "Minimum"
+  evaluation_periods  = 3
+  period              = 60
+  threshold           = 0.5
+  alarm_actions       = ["${var.sns}"]
+  dimensions = {
+    VpnId = var.vpn
+  }
+}
---
+++ cloudwatch-alarm-vpn/vars_local.tf
@@ -0,0 +1,3 @@
+#local variables
+variable "vpn" { default = "vpn-07939f0f25212bca8" } 
+variable "sns" { default = "arn:aws:sns:us-east-1:302762064522:deleteme" } 
