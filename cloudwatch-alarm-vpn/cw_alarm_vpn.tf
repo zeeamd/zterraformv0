6413a87b5c1ac2f44c8ba3e90d8c215591aed9e7
zee <zeeamd@dev.com>
2023-04-26 13:11:04 +0000

    update alarm

--- cloudwatch-alarm-vpn/cw_alarm_vpn.tf
+++ cloudwatch-alarm-vpn/cw_alarm_vpn.tf
@@ -8,7 +8,7 @@ resource "aws_cloudwatch_metric_alarm" "s2s_tunnel_status" {
   evaluation_periods  = 3
   period              = 60
   threshold           = 0.5
-  alarm_actions       = ["${var.sns}"]
+  alarm_actions       = [var.sns]
   dimensions = {
     VpnId = var.vpn
   }
--- cloudwatch-alarm-vpn/vars_local.tf
+++ cloudwatch-alarm-vpn/vars_local.tf
@@ -1,3 +1,3 @@
 #local variables
 variable "vpn" { default = "vpn-07939f0f25212bca8" } 
-variable "sns" { default = "arn:aws:sns:us-east-1:302762064522:deleteme" } 
+variable "sns" { default = "arn:aws:sns:us-east-1:12345678910:deleteme" } 
